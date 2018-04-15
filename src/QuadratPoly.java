d2840a02c1ff4b67194293cb15f329008d593d8f
maciej.holysz <maciejholysz@gmail.com>
2018-04-15 12:05:41 +0200

    Dodano obliczenie delty

--- src/QuadratPoly.java
+++ src/QuadratPoly.java
@@ -2,12 +2,14 @@ public class QuadratPoly {
     private int a;
     private int b;
     private int c;
+    private int delta;
 
 
     public QuadratPoly(int a, int b, int c){
         setA(a);
         setB(b);
         setC(c);
+        getDelta();
     }
 
     public int getA() {
@@ -32,6 +34,10 @@ public class QuadratPoly {
 
     public void setC(int c) {
         this.c = c;
+    }
+
+    private int getDelta() {
+        return getB()*getB() - 4 * getA()*getC();
     }
 
     public int value(int x){
