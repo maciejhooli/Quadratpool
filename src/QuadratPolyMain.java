81fa43790cd393870278579f60d455ca2112cb33
maciejhooli <maciejholysz@gmail.com>
2018-04-20 20:01:54 +0200

    Check out git connection

--- src/QuadratPolyMain.java
+++ src/QuadratPolyMain.java
@@ -2,6 +2,6 @@ public class QuadratPolyMain {
 
     public static void main(String[] args) {
         QuadratPoly poly = new QuadratPoly(1,2,3);
-        System.out.println(poly.value(2));
+        System.out.println(poly.value(3));
     }
 }
