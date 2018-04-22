9d94bc825668992aee0720fca3df165262203db1
maciejhooli <maciejholysz@gmail.com>
2018-04-22 22:01:54 +0200

    Zadanie 3
    Metoda to string

--- src/QuadratPoly.java
+++ src/QuadratPoly.java
@@ -5,7 +5,7 @@ public class QuadratPoly {
     private int delta;
 
 
-    public QuadratPoly(int a, int b, int c){
+    public QuadratPoly(int a, int b, int c) {
         setA(a);
         setB(b);
         setC(c);
@@ -37,11 +37,51 @@ public class QuadratPoly {
     }
 
     private int getDelta() {
-        return getB()*getB() - 4 * getA()*getC();
+        return getB() * getB() - 4 * getA() * getC();
     }
 
-    public int value(int x){
-        return getA()*x^2 + getB() * x + getC();
+    public int value(int x) {
+        return getA() * x ^ 2 + getB() * x + getC();
     }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        if ( getA() == -1 ) {
+            sb.append("-x^2");
+        } else if ( getA() == 1 ) {
+            sb.append("x^2");
+        } else if ( getA() > 0 || getA() < 0 ) {
+            sb.append(getA()).append("x^2");
+        }
+
+        if ( getA() != 0 ) {
+            if ( getB() == -1 ) {
+                sb.append("-x");
+            } else if ( getB() == 1 ) {
+                sb.append("+").append("x");
+            } else if ( getB() < 0 ) {
+                sb.append(getB()).append("x");
+            } else if ( getB() > 0 ) {
+                sb.append("+").append(getB()).append("x");
+            }
+        } else {
+            if ( getB() == -1 ) {
+                sb.append("-x");
+            } else if ( getB() == 1 ) {
+                sb.append("x");
+            } else if (getB() < 0 || getB() > 0) {
+                sb.append(getB()).append("x");
+            }
+        }
+
+        if ( getC() > 0 ) {
+            sb.append("+").append(getC());
+        } else if ( getC() < 0 ) {
+            sb.append(getC());
+        }
+
+        return sb.toString();
+    }
 }
--- src/QuadratPolyMain.java
+++ src/QuadratPolyMain.java
@@ -1,7 +1,8 @@
 public class QuadratPolyMain {
 
     public static void main(String[] args) {
-        QuadratPoly poly = new QuadratPoly(1,2,3);
+        QuadratPoly poly = new QuadratPoly(2,8,1);
         System.out.println(poly.value(3));
+        System.out.println(poly);
     }
 }
